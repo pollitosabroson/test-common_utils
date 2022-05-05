c14e100e58cf3714a11d0aa07a35da0e804cae16
Alejandro <alejandro.hernandez@neutroon.com>
2022-05-05 17:36:07 +0200

    Integrate folder for utils

---
+++ common_utils/__init__.py
@@ -0,0 +1,5 @@
+"""common_utils - Functionalities that are common within our projects"""
+
+__version__ = "0.0.1"
+__author__ = "Alejandro hernandez <alejandro.hernandez@neutroon.com>"
+__all__ = []
---
+++ common_utils/utils/constants.py
@@ -0,0 +1,4 @@
+ID_NOT_FOUND = "ID not found"
+ERROR_COLLECTION_EMPTY = "The collection name cannot be empty"
+VALUE_DONT_EXISTS = "The value you are looking for does not exist"
+DELETE_NOT_ALLOW = "Delete all not allow"
